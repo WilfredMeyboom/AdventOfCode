873c87137b5ab3e1eecaac54110d5249fbeb6818
Wilfred Meyboom <wilfred.meyboom@gmail.com>
2020-08-12 10:58:45 +0200

    Year 2018 complete

--- 2018/Day21.sql
+++ 2018/Day21.sql
@@ -5,7 +5,7 @@ SET NOCOUNT ON
 CREATE TABLE ##Input (Line NVARCHAR(MAX));
 
 BULK INSERT ##Input
-FROM 'C:\Source\AdventOfCode\2018\input19.txt'
+FROM 'C:\Source\AdventOfCode\2018\input21.txt'
 WITH (ROWTERMINATOR = '0x0A');
 
 DECLARE @IPReg INT
@@ -18,34 +18,36 @@ CREATE TABLE ##Program (InstrNr INT IDENTITY(0,1) PRIMARY KEY, Instr CHAR(4), A 
 
 INSERT ##Program (Instr, A, B, C)
 SELECT LEFT(Line, 4) AS Instr
-,      LEFT(SUBSTRING(Line, 6, LEN(Line)), 1) --All first instructions are 1 digit
-,      SUBSTRING(Line, 7, 3)
-,      RIGHT(Line, 1) --Only 6 Registers so this works
+,      LEFT(SUBSTRING(Line, 6, LEN(Line)), CHARINDEX(' ', SUBSTRING(Line, 6, LEN(Line)))) AS A
+,      REVERSE(LEFT(LTRIM(REVERSE(LEFT(Line, LEN(Line) - 1))), CHARINDEX(' ', LTRIM(REVERSE(LEFT(Line, LEN(Line) - 1)))))) AS B
+,      RIGHT(Line, 1) AS C--Only 6 Registers so this works
 FROM ##Input
 
---SELECT * FROM ##Program
+--SELECT * FROM ##Program 
 
 CREATE TABLE ##Register (Nr INT IDENTITY(0,1) PRIMARY KEY, Val BIGINT)
 
 INSERT ##Register (Val) SELECT TOP 6 0 FROM sys.messages
-
+UPDATE ##Register SET Val = 10199686 WHERE Nr = 0
 --SELECT * FROM ##Register
+
 
 DECLARE @ProgramSize INT
 SELECT @ProgramSize = COUNT(1) FROM ##Program
 
 DECLARE @IP INT = 0 --InstructionPointer
-DECLARE @A INT
-DECLARE @B INT
-DECLARE @C INT
+DECLARE @A BIGINT
+DECLARE @B BIGINT
+DECLARE @C BIGINT
 DECLARE @Instr CHAR(4)
 
-DECLARE @RegA INT
-DECLARE @RegB INT
+DECLARE @RegA BIGINT
+DECLARE @RegB BIGINT
 DECLARE @Debug INT = 1
 DECLARE @Counter BIGINT = 0
 
-DECLARE @DoPart2 INT = 1
+DECLARE @DoPart2 INT = 0
+
 
 IF @DoPart2 = 1 UPDATE ##Register SET Val = 1 WHERE Nr = 0
 
@@ -69,7 +71,7 @@ BEGIN
 
         SET @Counter = @Counter + 1
 
-        PRINT 'Count:' + CAST(@Counter AS VARCHAR(50)) + '|' + @Instr + '|A:' + CAST(@A AS VARCHAR(20)) + '|B:' + CAST(@B AS VARCHAR(20)) + '|RegA:' + CAST(@RegA AS VARCHAR(20)) + '|RegB:' + ISNULL(CAST(@RegB AS VARCHAR(20)),'*') + '|C:' + CAST(@C AS VARCHAR(20)) + '|IP:' + CAST(@IP AS VARCHAR(20))
+        PRINT 'Count:' + CAST(@Counter AS VARCHAR(50)) + '|' + @Instr + '|A:' + CAST(@A AS VARCHAR(20)) + '|B:' + CAST(@B AS VARCHAR(20)) + '|RegA:' + ISNULL(CAST(@RegA AS VARCHAR(20)), '*') + '|RegB:' + ISNULL(CAST(@RegB AS VARCHAR(20)),'*') + '|C:' + CAST(@C AS VARCHAR(20)) + '|IP:' + CAST(@IP AS VARCHAR(20))
     END
 
     --addr (add register) stores into register C the result of adding register A and register B.
@@ -208,34 +210,103 @@ BEGIN
     -- Keep register and IP in sync
     SELECT @IP = Val FROM ##Register WHERE Nr = @IPReg
 
+    IF @IP = 28 AND @DoPart2 = 0 
+    BEGIN
+        PRINT 'First check on:' + CAST(@RegA AS VARCHAR(20))
+        --SET @IP = 9999
+    END
+
+    --IF @IP = 28 AND @DoPart2 = 1
+    --BEGIN
+    --    INSERT ##Solutions (Sol) SELECT @RegA
+    --END
+    
+
     SET @IP = @IP + 1
 
 END
 
-SELECT * FROM ##Register
+--SELECT * FROM ##Register
 
--- 1092 is correct for part 1 (Took 21min)
+--SELECT * FROM ##Program WHERE A = 0 OR B = 0 --> Instr 28 is key
+
+-- 15823996 is correct for part 1
 
 /*
 
 DROP TABLE ##Register
 DROP TABLE ##Program
 DROP TABLE ##Input
+DROP TABLE ##Solutions
 
 */
 
-/*
-Unfortunaly part 2 cannot be done by simply running the program. It would take far too long.
+-- As expected. Part 2 cannot be done by simply running the code
 
-After analysing this is what the program does:
-It has a target number (10551305 in our case)
-It sums all divisors for this number
+/* This is the program in psuedo code
+R4 = 0
 
-Divisors for 10551305 are
-1, 5, 17, 85, 124133, 620665, 2110261, 10551305
-They're sum is 13406472
+6   R3 = R4 | 65536
+7   R4 = 16098955
+8&9 R4 = R4 + R3 & 255
+10  R4 = R4 & 16777215
+11  R4 = R4 * 65899
+12  R4 = R4 & 16777215
 
-Which is correct for part 2
+13 IF (256 > R3) 
+   THEN JUMP TO 28 
+   ELSE R3 = (R3 / 256) + 1 AND JUMP to 8
 
+28 IF R4 == R0 EXIT ELSE GOTO 6
 */
+
+DECLARE @R0 BIGINT = 0 
+DECLARE @R3 BIGINT = 0
+DECLARE @R4 BIGINT = 0 
+
+DECLARE @ContinuProgram INT = 1
+
+SET @R3 = @R4 | 65536
+SET @R4 = 16098955
+
+CREATE TABLE ##Solutions (ID INT IDENTITY(1,1), Sol BIGINT)
+
+WHILE @ContinuProgram = 1
+BEGIN
+    
+    SET @R4 = (((@R4 + (@R3 & 255)) & 16777215) * 65899) & 16777215
+
+    --PRINT @R3
+    --PRINT @R4
+
+    IF @R3 < 256 
+    BEGIN
+        INSERT ##Solutions (Sol) SELECT @R4
+
+        SET @R3 = @R4 | 65536
+        SET @R4 = 16098955
+
+        IF (SELECT COUNT(1) FROM ##Solutions) > 100000 SET @ContinuProgram = 0
+    END
+    ELSE 
+    BEGIN
+        SET @R3 = (@R3 / 256)
+    END
+
+END
+
+-- Do the same solutions start popping up?
+SELECT * FROM ##Solutions S1
+INNER JOIN ##Solutions S2 ON S1.Sol = S2.Sol AND S1.ID < S2.ID
+ORDER BY 1, 3
+-- Yes, they do
+
+-- This seems to be one cycle of solutions
+SELECT * FROM ##Solutions WHERE ID BETWEEN 10330 AND 11458 ORDER BY 1
+
+-- So the last one is the request will take the longest time (i.e. the most instructions) to reach
+SELECT * FROM ##Solutions WHERE ID = (11458 - 1)
+
+-- 10199686 is correct for part 2
+
 
