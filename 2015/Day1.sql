68529bfa208f97f63fe2b449b274c3783509cc59
Wilfred Meyboom <wilfred.meyboom@gmail.com>
2021-12-05 15:56:17 +0100

    Updating older solutions 2015 day 1

--- 2015/Day1.sql
+++ 2015/Day1.sql
@@ -1,45 +1,42 @@
-use Test_WME
+USE Test_WME
 
-SET NOCOUNT ON
+DECLARE @year VARCHAR(4) = '2015'
+DECLARE @day VARCHAR(2)  = '1'
 
-CREATE TABLE ##Input (Line NVARCHAR(MAX));
+EXEC dbo.GetInput @year = @year, @day = @day
+EXEC dbo.ParseInput @year = @year, @day = @day 
 
-BULK INSERT ##Input
-FROM 'D:\Wilfred\AdventOfCode\2015\input1.txt'
-WITH (ROWTERMINATOR = '0x0A');
+SELECT TOP 10 * FROM ##InputNumbered
+SELECT TOP 10 * FROM ##InputGrid
+SELECT TOP 10 * FROM ##InputInts
+SELECT TOP 10 * FROM ##InputSplit
 
-SELECT *, LEN(Line), LEN(REPLACE(Line, '(', '')) FROM ##Input
-
-SELECT 7000 - (3384*2)
+;WITH cte_Amt AS (
+    SELECT Val, COUNT(1) AS Amt
+    FROM ##InputGrid
+    GROUP BY Val
+)
+SELECT ABS(C1.Amt-C2.Amt) Part1
+FROM cte_Amt C1
+INNER JOIN cte_Amt C2 ON C1.Val > C2.Val
 
 -- 232 is correct for part 1
 
-
 ;WITH cte_Floors AS (
-    SELECT 0 AS PreviousFloor
-    ,      CASE WHEN LEFT(Line, 1) = '(' THEN 1 ELSE -1 END AS CurrentFloor
-    ,      SUBSTRING(Line, 2, LEN(Line)) AS Remainder
-    FROM ##Input
-    UNION ALL
-    SELECT CurrentFloor AS PreviousFloor
-    ,      CurrentFloor + CASE WHEN LEFT(Remainder, 1) = '(' THEN 1 ELSE -1 END AS CurrentFloor
-    ,      SUBSTRING(Remainder, 2, LEN(Remainder)) AS Remainder
+    SELECT ColNr + 1 AS ColNr    -- Character counting starts at 1
+    , CASE WHEN Val = '(' THEN 1 ELSE -1 END AS IntVal
+    FROM ##InputGrid
+), cte_RunningTotal AS (
+    SELECT ColNr, SUM(IntVal) OVER (ORDER BY ColNr) AS FloorNr
     FROM cte_Floors
-    WHERE LEN(Remainder) > 0
-
 )
-SELECT ROW_NUMBER() OVER (ORDER BY (SELECT 0)), *, LEN(Remainder)
-FROM cte_Floors
---WHERE CurrentFloor = -1
-OPTION (MAXRECURSION 8000)
+SELECT MIN(ColNr) AS Part2
+FROM cte_RunningTotal
+WHERE FloorNr = -1
 
 
 --1783 is correct for part 2
 
-/*
 
-DROP TABLE ##Input
-
-*/
 
 
