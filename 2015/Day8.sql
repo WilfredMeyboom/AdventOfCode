574248692a7ffbfb52ce5b60e4ba30e549cb8cfa
Wilfred Meyboom <A-Wilfred.Meyboom@paccar.com>
2022-11-30 12:19:33 +0100

    Update 2015 day 6 through 9

--- 2015/Day8.sql
+++ 2015/Day8.sql
@@ -1,15 +1,12 @@
-use Test_WME
+USE Test_WME
 
 SET NOCOUNT ON
 
-CREATE TABLE ##Input (Line NVARCHAR(MAX));
+DECLARE @year VARCHAR(4) = '2015'
+DECLARE @day VARCHAR(2)  = '8'
 
-BULK INSERT ##Input
-FROM 'D:\Wilfred\AdventOfCode\2015\input8.txt'
-WITH (ROWTERMINATOR = '0x0A');
-
---SELECT * FROM ##Input
---SELECT SUM(LEN(Line)) FROM ##Input
+EXEC dbo.GetInput @year = @year, @day = @day
+EXEC dbo.ParseInput @year = @year, @day = @day 
 
 ;WITH cte_RemoveQuotes AS (
     SELECT ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS RowNr
@@ -19,7 +16,7 @@ WITH (ROWTERMINATOR = '0x0A');
     FROM ##Input
 ), cte_Escaped AS (
     SELECT RowNr
-    , REPLACE(REPLACE(REPLACE(Line, '\\x', '\\*'), '\"', '"'), '\\', '\') AS Line
+    , REPLACE(REPLACE(Line, '\"', '"'), '\\', '/') AS Line
     , OriginalLength
     , OriginalLine
     FROM cte_RemoveQuotes
@@ -27,49 +24,33 @@ WITH (ROWTERMINATOR = '0x0A');
     SELECT RowNr
     ,   1 AS Iteration
     ,   CASE WHEN CHARINDEX('\x', Line) > 0 THEN 
-            REPLACE(Line, SUBSTRING(Line, CHARINDEX('\x', Line), 4), CHAR(CONVERT(INT, CONVERT(VARBINARY, SUBSTRING(Line, CHARINDEX('\x', Line) + 2, 2), 2)))) ELSE Line END AS Line
-    ,       OriginalLength
+            REPLACE(Line, SUBSTRING(Line, CHARINDEX('\x', Line), 4), CHAR(CONVERT(INT, CONVERT(VARBINARY, SUBSTRING(Line, CHARINDEX('\x', Line) + 2, 2), 2)))) 
+            ELSE Line END AS Line
+    ,   OriginalLength
     ,   OriginalLine
+    ,   Line AS EscapedLine
     FROM cte_Escaped
     UNION ALL
     SELECT RowNr
     ,   Iteration + 1
     ,   CASE WHEN CHARINDEX('\x', Line) > 0 THEN 
             REPLACE(Line, SUBSTRING(Line, CHARINDEX('\x', Line), 4), CHAR(CONVERT(INT, CONVERT(VARBINARY, SUBSTRING(Line, CHARINDEX('\x', Line) + 2, 2), 2)))) ELSE Line END AS Line
-    ,       OriginalLength
+    ,   OriginalLength
     ,   OriginalLine
+    ,   Line AS EscapedLine
     FROM cte_Hex
     WHERE CHARINDEX('\x', Line) > 0 
-)
-SELECT *, LEN(REPLACE(REPLACE(REPLACE(Line, '\\x', '\\*'), '\"', '"'), '\\', '\')), REPLACE(REPLACE(REPLACE(Line, '\\x', '\\*'), '\"', '"'), '\\', '\')
---SUM(OriginalLength), SUM(LEN(Line))
+) 
+SELECT --*, LEN(Line) AS NewLength
+SUM(OriginalLength) AS TotalOriginalLength, SUM(LEN(Line)) AS TotalNewLength, SUM(OriginalLength) - SUM(LEN(Line)) AS Part1
 FROM cte_Hex cH
 INNER JOIN (SELECT RowNr, MAX(Iteration) AS MaxIter FROM cte_Hex GROUP BY RowNr) Sub ON cH.RowNr = Sub.RowNr AND cH.Iteration = Sub.MaxIter
---ORDER BY cH.RowNr, cH.Iteration
-WHERE Line LIKE N'%*%'
-
-SELECT 6489 - (5127 - 9) --Correctie voor \\*, maar geen iteraties daarna
-SELECT 6489 - 5114 
-
--- 1362 is too low for Part 1
--- 1375 is too high for Part 1
--- 1384 is too high for Part 1
 
 -- 1371 is correct for Part 1
 
 
-SELECT SUM(LEN('"' + REPLACE(REPLACE(Line, '\', '\\'), '"', '\"') + '"'))
+SELECT SUM(LEN(Line)) AS OriginalLength, SUM(LEN('"' + REPLACE(REPLACE(Line, '\', '\\'), '"', '\"') + '"')) AS NewLength
+, SUM(LEN('"' + REPLACE(REPLACE(Line, '\', '\\'), '"', '\"') + '"')) - SUM(LEN(Line)) AS Part2
 FROM ##Input
 
-SELECT 8606 - 6489
-
 --2117 is correct for part 2
-
-/*
-
-DROP TABLE ##Input
-
-
-*/
-
-
