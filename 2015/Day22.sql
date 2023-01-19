e8834ade03a7b1fc2c598b110bf7d249a98e5abb
Wilfred Meyboom <A-Wilfred.Meyboom@paccar.com>
2023-01-19 11:24:33 +0100

    2015 Day 22 Cleaned up <phew>

--- 2015/Day22.sql
+++ 2015/Day22.sql
@@ -20,115 +20,204 @@ INSERT ##Spells (Name, ManaCost, Duration, Damage, Armor, Health, Mana) VALUES
 ('MagicMissile', 53, 1, 4, 0, 0, 0),
 ('Drain'       , 73, 1, 2, 0, 2, 0),
 ('Shield'     , 113, 6, 0, 7, 0, 0),
-('Poison'     , 173, 6, 3, 0, 0, 0),
-('Recharge'   , 229, 5, 2, 0, 0, 101)
+('Poison'     , 173, 6, 0, 0, 0, 0), -- Poisondamage is tracked through a variable
+('Recharge'   , 229, 5, 0, 0, 0, 101)
 
 
-CREATE TABLE ##FightStatus (ID INT IDENTITY(1,1), PreviousID INT, Round INT, PlayerHealth INT, PlayerMana INT, BossHealth INT, ShieldActive INT, ShieldValue INT, PoisonActive INT, PoisonValue INT, RechargeActive INT, RechargeValue INT, ManaSpent INT)
+CREATE TABLE ##FightStatus (ID INT IDENTITY(1,1), RoundNr INT, PlayerHealth INT, PlayerMana INT, BossHealth INT, ShieldActive INT, PoisonActive INT, RechargeActive INT, ManaSpent INT, SpellsPath VARCHAR(MAX))
 
-INSERT ##FightStatus (Round, PlayerHealth, PlayerMana, BossHealth, ShieldActive, ShieldValue, PoisonActive, PoisonValue, RechargeActive, RechargeValue, ManaSpent) VALUES (0, 50, 500, 55, 0, 7, 0, 3, 0, 101, 0)
---INSERT ##FightStatus (Round, PlayerHealth, PlayerMana, BossHealth, ShieldActive, ShieldValue, PoisonActive, PoisonValue, RechargeActive, RechargeValue) VALUES (0, 10, 250, 14, 0, 7, 0, 3, 0, 101)
-
-CREATE TABLE ##HistoricFightStatus (ID INT, PreviousID INT, Round INT, PlayerHealth INT, PlayerMana INT, BossHealth INT, ShieldActive INT, ShieldValue INT, PoisonActive INT, PoisonValue INT, RechargeActive INT, RechargeValue INT, ManaSpent INT)
+INSERT ##FightStatus (RoundNr, PlayerHealth, PlayerMana, BossHealth, ShieldActive, PoisonActive, RechargeActive, ManaSpent, SpellsPath) VALUES (0, 50, 500, 55, 0, 0, 0, 0, '')
+--INSERT ##FightStatus (RoundNr, PlayerHealth, PlayerMana, BossHealth, ShieldActive, PoisonActive, RechargeActive, ManaSpent, SpellsPath) VALUES (0, 10, 250, 14, 0, 0, 0, 0, '')
 
 DECLARE @BossDamage INT = 8
 DECLARE @Round INT = 0
+DECLARE @ShieldValue INT = 7
+DECLARE @PoisonValue INT = 3
+DECLARE @RechargeValue INT = 101
 
-WHILE EXISTS (SELECT 1 FROM ##FightStatus) AND @Round < 13
+WHILE NOT EXISTS (SELECT 1 FROM ##FightStatus WHERE BossHealth <= 0) AND @Round < 25
 BEGIN
     
     SET @Round = @Round + 1
 
-    -- Store what happened
-    INSERT ##HistoricFightStatus SELECT * FROM ##FightStatus
-
-    -- Change for part 2
-    UPDATE ##FightStatus SET PlayerHealth = PlayerHealth - 1
-
-    -- Remove any fights that are over due to the player dying, the boss dying or the player running out of mana
-    DELETE FROM ##FightStatus
-    WHERE PlayerHealth <= 0 OR BossHealth <= 0 OR PlayerMana <= 0
-   
-    INSERT ##FightStatus (Round, PreviousID, PlayerHealth, PlayerMana, BossHealth, ShieldActive, ShieldValue, PoisonActive, PoisonValue, RechargeActive, RechargeValue, ManaSpent)
+    INSERT ##FightStatus (RoundNr, PlayerHealth, PlayerMana, BossHealth, ShieldActive, PoisonActive, RechargeActive, ManaSpent, SpellsPath)
     SELECT @Round
-    ,      F.ID AS PreviousID
-    ,      F.PlayerHealth - CASE WHEN (F.ShieldActive > 1 OR S.Armor > 0)         -- Is the shield (just) activated?
-                                 THEN CASE WHEN F.ShieldValue - @BossDamage > 1   -- Does the boss do more damage than our shield
-                                      THEN F.ShieldValue - @BossDamage            -- Take the difference in damage
-                                      ELSE 1 END                                  -- Always take at least 1 damage
-                                 ELSE @BossDamage END                             -- Take the full boss damage
-                          + S.Health AS PlayerHealth
-    ,      F.PlayerMana - S.ManaCost + CASE WHEN F.RechargeActive > 1 OR S.Mana > 0 
-                                            THEN 2*F.RechargeValue 
-                                            ELSE CASE WHEN F.RechargeActive = 1 
-                                                      THEN F.RechargeValue
-                                                      ELSE 0 
-                                                      END
+    ,      F.PlayerHealth + S.Health AS PlayerHealth                                          -- If we just cast Drain
+    ,      F.PlayerMana - S.ManaCost + CASE WHEN F.RechargeActive > 1                         -- Has recharge been cast? 
+                                            THEN @RechargeValue                               -- We recharge twice (once during player turn and once during boss turn)
+                                            ELSE 0
                                         END AS PlayerMana
-    ,      F.BossHealth - S.Damage - CASE WHEN F.PoisonActive > 1 
-                                          THEN 2*F.PoisonValue 
-                                          ELSE CASE WHEN F.PoisonActive = 1 
-                                                    THEN F.PoisonValue
-                                                    ELSE 0 
-                                                    END
+    ,      F.BossHealth - S.Damage - CASE WHEN F.PoisonActive > 1                             -- Has poison (just) been cast? 
+                                          THEN @PoisonValue 
+                                          ELSE 0 
                                           END AS BossHealth
     ,      CASE WHEN S.Name = 'Shield' 
-                THEN S.Duration - 2 
+                THEN S.Duration + 1                 --Correction because SQL does everything at once
                 ELSE CASE WHEN F.ShieldActive > 0
-                          THEN F.ShieldActive - 2
+                          THEN F.ShieldActive - 1
                           ELSE 0 END
                 END AS ShieldActive
-    ,      ShieldValue
     ,      CASE WHEN S.Name = 'Poison' 
-                THEN S.Duration - 2 
+                THEN S.Duration + 1                 --Correction because SQL does everything at once
                 ELSE CASE WHEN F.PoisonActive > 0 
-                          THEN F.PoisonActive - 2
+                          THEN F.PoisonActive - 1
                           ELSE 0 END                
                 END AS PoisonActive
-    ,      PoisonValue
     ,      CASE WHEN S.Name = 'Recharge' 
-                THEN S.Duration - 2 
+                THEN S.Duration + 1                 --Correction because SQL does everything at once
                 ELSE CASE WHEN F.RechargeActive > 0 
-                          THEN F.RechargeActive - 2
+                          THEN F.RechargeActive - 1
                           ELSE 0 END
                 END AS RechargeActive
-    ,      RechargeValue
     ,      ManaSpent + S.ManaCost AS ManaSpent
+    ,      SpellsPath + ' | ' + S.Name
     FROM ##FightStatus F
     CROSS APPLY ##Spells S
-    WHERE NOT ((F.PoisonActive > 0 AND S.Name = 'Poison') OR
-               (F.ShieldActive > 0 AND S.Name = 'Shield') OR
-               (F.RechargeActive > 0 AND S.Name = 'Recharge'))
-               
-    DELETE FROM ##FightStatus WHERE Round = @Round - 1
+    WHERE NOT ((F.PoisonActive > 2 AND S.Name = 'Poison') OR
+               (F.ShieldActive > 1 AND S.Name = 'Shield') OR
+               (F.RechargeActive > 1 AND S.Name = 'Recharge'))
+              
+    DELETE FROM ##FightStatus WHERE PlayerHealth <= 0 OR PlayerMana <= 0
 
-PRINT 'Round: ' + CAST(@Round AS VARCHAR(6)) 
+    SET @Round = @Round + 1
+
+    UPDATE F
+    SET RoundNr = @Round
+    ,   PlayerHealth = F.PlayerHealth - CASE WHEN F.ShieldActive > 1                          -- Is the shield activated?
+                                             THEN CASE WHEN @ShieldValue - @BossDamage > 1    -- Does the boss do more damage than our shield
+                                                       THEN @ShieldValue - @BossDamage        -- Take the difference in damage
+                                                       ELSE 1 END                             -- Always take at least 1 damage
+                                             ELSE @BossDamage END                             -- Take the full boss damage
+    ,  PlayerMana = F.PlayerMana + CASE WHEN F.RechargeActive > 1                             -- Has recharge been cast? 
+                                        THEN @RechargeValue                                   -- We recharge twice (once during player turn and once during boss turn)
+                                        ELSE 0
+                                        END 
+    ,  BossHealth = F.BossHealth - CASE WHEN F.PoisonActive > 1                               -- Has poison (just) been cast? 
+                                          THEN @PoisonValue 
+                                          ELSE 0 
+                                          END 
+    ,  ShieldActive = CASE WHEN F.ShieldActive > 0
+                           THEN F.ShieldActive - 1
+                           ELSE 0 END
+    ,  PoisonActive = CASE WHEN F.PoisonActive > 0 
+                           THEN F.PoisonActive - 1
+                           ELSE 0 END                
+    ,  RechargeActive = CASE WHEN F.RechargeActive > 0 
+                             THEN F.RechargeActive - 1
+                             ELSE 0 END
+    FROM ##FightStatus F
+    WHERE BossHealth > 0 AND RoundNr = @Round - 1
+
+    DELETE FROM ##FightStatus WHERE PlayerHealth <= 0 OR PlayerMana <= 0
+
+    DELETE FROM ##FightStatus WHERE RoundNr = @Round - 2
+
+    PRINT 'Round: ' + CAST(@Round AS VARCHAR(6)) 
 
 END
 
--- 900 is too low for part 1
--- 953 is correct for part 1
+SELECT MIN(ManaSpent) AS Part1 FROM ##FightStatus WHERE BossHealth <= 0
 
--- 1066 is too low for part 2
--- 1289 is correct for part 2
-/*
+
+--Reset for part2
+TRUNCATE TABLE ##FightStatus
+
+INSERT ##FightStatus (RoundNr, PlayerHealth, PlayerMana, BossHealth, ShieldActive, PoisonActive, RechargeActive, ManaSpent, SpellsPath) VALUES (0, 50, 500, 55, 0, 0, 0, 0, '')
+
+SET @Round = 0
+
+WHILE NOT EXISTS (SELECT 1 FROM ##FightStatus WHERE BossHealth <= 0) AND @Round < 25
+BEGIN
+
+    -- Change for part 2
+    UPDATE ##FightStatus SET PlayerHealth = PlayerHealth - 1 WHERE RoundNr = @Round 
+
+    SET @Round = @Round + 1
+   
+    INSERT ##FightStatus (RoundNr, PlayerHealth, PlayerMana, BossHealth, ShieldActive, PoisonActive, RechargeActive, ManaSpent, SpellsPath)
+    SELECT @Round
+    ,      F.PlayerHealth + S.Health AS PlayerHealth                                          -- If we just cast Drain
+    ,      F.PlayerMana - S.ManaCost + CASE WHEN F.RechargeActive > 1                         -- Has recharge been cast? 
+                                            THEN @RechargeValue                               -- We recharge twice (once during player turn and once during boss turn)
+                                            ELSE 0
+                                        END AS PlayerMana
+    ,      F.BossHealth - S.Damage - CASE WHEN F.PoisonActive > 1                             -- Has poison (just) been cast? 
+                                          THEN @PoisonValue 
+                                          ELSE 0 
+                                          END AS BossHealth
+    ,      CASE WHEN S.Name = 'Shield' 
+                THEN S.Duration + 1                 --Correction because SQL does everything at once
+                ELSE CASE WHEN F.ShieldActive > 0
+                          THEN F.ShieldActive - 1
+                          ELSE 0 END
+                END AS ShieldActive
+    ,      CASE WHEN S.Name = 'Poison' 
+                THEN S.Duration + 1                  --Correction because SQL does everything at once
+                ELSE CASE WHEN F.PoisonActive > 0 
+                          THEN F.PoisonActive - 1
+                          ELSE 0 END                
+                END AS PoisonActive
+    ,      CASE WHEN S.Name = 'Recharge' 
+                THEN S.Duration + 1                 --Correction because SQL does everything at once
+                ELSE CASE WHEN F.RechargeActive > 0 
+                          THEN F.RechargeActive - 1
+                          ELSE 0 END
+                END AS RechargeActive
+    ,      ManaSpent + S.ManaCost AS ManaSpent
+    ,      SpellsPath + ' | ' + S.Name
+    FROM ##FightStatus F
+    CROSS APPLY ##Spells S
+    WHERE NOT ((F.PoisonActive > 2 AND S.Name = 'Poison') OR
+               (F.ShieldActive > 1 AND S.Name = 'Shield') OR
+               (F.RechargeActive > 1 AND S.Name = 'Recharge'))
+               
+    DELETE FROM ##FightStatus WHERE PlayerHealth <= 0 OR PlayerMana <= 0
+    DELETE FROM ##FightStatus WHERE RoundNr = @Round - 1
+
+
+    SET @Round = @Round + 1
+
+    UPDATE F
+    SET RoundNr = @Round
+    ,   PlayerHealth = F.PlayerHealth - CASE WHEN F.ShieldActive > 1                          -- Is the shield activated?
+                                             THEN CASE WHEN @ShieldValue - @BossDamage > 1    -- Does the boss do more damage than our shield
+                                                       THEN @ShieldValue - @BossDamage        -- Take the difference in damage
+                                                       ELSE 1 END                             -- Always take at least 1 damage
+                                             ELSE @BossDamage END                             -- Take the full boss damage
+    ,  PlayerMana = F.PlayerMana + CASE WHEN F.RechargeActive > 1                             -- Has recharge been cast? 
+                                        THEN @RechargeValue                                   -- We recharge twice (once during player turn and once during boss turn)
+                                        ELSE 0
+                                        END 
+    ,  BossHealth = F.BossHealth - CASE WHEN F.PoisonActive > 1                               -- Has poison (just) been cast? 
+                                          THEN @PoisonValue 
+                                          ELSE 0 
+                                          END 
+    ,  ShieldActive = CASE WHEN F.ShieldActive > 0
+                           THEN F.ShieldActive - 1
+                           ELSE 0 END
+    ,  PoisonActive = CASE WHEN F.PoisonActive > 0 
+                           THEN F.PoisonActive - 1
+                           ELSE 0 END                
+    ,  RechargeActive = CASE WHEN F.RechargeActive > 0 
+                             THEN F.RechargeActive - 1
+                             ELSE 0 END
+    FROM ##FightStatus F
+    WHERE BossHealth > 0 AND RoundNr = @Round - 1
+
+    DELETE FROM ##FightStatus WHERE PlayerHealth <= 0 OR PlayerMana <= 0
+
+    DELETE FROM ##FightStatus WHERE RoundNr = @Round - 2
+
+    PRINT 'Round: ' + CAST(@Round AS VARCHAR(6)) 
+
+
+END
+
+SELECT MIN(ManaSpent) AS Part2 FROM ##FightStatus WHERE BossHealth <= 0
 
 DROP TABLE ##FightStatus
-DROP TABLE ##HistoricFightStatus
+
 DROP TABLE ##Spells
 
-*/
 
---SELECT * FROM ##HistoricFightStatus WHERE Round = 1
-----ID 5
---SELECT * FROM ##HistoricFightStatus WHERE PreviousID = 6
 
---SELECT * FROM ##HistoricFightStatus WHERE PreviousID = 27
-
---SELECT * FROM ##HistoricFightStatus WHERE PreviousID = 49
-
---SELECT * FROM ##HistoricFightStatus WHERE PreviousID = 85
-
---SELECT * FROM ##FightStatus
-
---SELECT * FROM ##HistoricFightStatus WHERE BossHealth <= 0 ORDER BY Round
