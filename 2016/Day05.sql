af8d496cf06241e351924e6dc85a88a2ede8d449
Wilfred Meyboom <A-Wilfred.Meyboom@paccar.com>
2023-01-30 14:59:45 +0100

    2016 Day 5 & 6 Cleaned up

--- 2016/Day05.sql
+++ 2016/Day05.sql
@@ -1,43 +1,48 @@
-
---SELECT LEFT(sys.fn_sqlvarbasetostr(HASHBYTES('MD5', 'abc5278568')), 7)
-
+SET NOCOUNT ON
 
 DECLARE @RoomID VARCHAR(10) = 'ojvtpuvg'
 DECLARE @Counter BIGINT = 0
 
 
-CREATE TABLE ##Hashes (ID INT IDENTITY(1,1), Counter BIGINT, Hash VARCHAR(50))
+CREATE TABLE ##Hashes (ID INT IDENTITY(1,1), Cnter BIGINT, Hash VARCHAR(50), Pos CHAR)
 
---1469591
---1925351
-
-SET @Counter = 1469591
-INSERT ##Hashes (Counter, Hash) VALUES (@Counter, sys.fn_sqlvarbasetostr(HASHBYTES('MD5', @RoomID + CAST(@Counter AS VARCHAR(15)))))
-SET @Counter = 1925351
--- This counter will work but it will be added through the regular process
---INSERT ##Hashes (Counter, Hash) VALUES (@Counter, sys.fn_sqlvarbasetostr(HASHBYTES('MD5', @RoomID + CAST(@Counter AS VARCHAR(15)))))
-
-
-WHILE (SELECT COUNT(DISTINCT SUBSTRING(Hash, 8, 1)) FROM ##Hashes WHERE SUBSTRING(Hash, 8, 1) IN ('0','1','2','3','4','5','6','7')) < 8
+WHILE (SELECT COUNT(DISTINCT Pos) FROM ##Hashes WHERE Pos BETWEEN '0' AND '7') < 8
 BEGIN
 
-    IF (SELECT LEFT(sys.fn_sqlvarbasetostr(HASHBYTES('MD5', @RoomID + CAST(@Counter AS VARCHAR(15)))), 7)) = '0x00000'
+    IF (SELECT CAST(CAST(HASHBYTES('MD5', @RoomID + CAST(@Counter AS VARCHAR(15))) AS VARBINARY(4)) AS INT)) BETWEEN 0 AND 4095
     BEGIN
-        PRINT 'Correct hash found at ' + CAST(GETDATE() AS VARCHAR(20)) + ' with counter at ' + CAST(@Counter AS VARCHAR(15))
        
-        INSERT ##Hashes (Counter, Hash) VALUES (@Counter, sys.fn_sqlvarbasetostr(HASHBYTES('MD5', @RoomID + CAST(@Counter AS VARCHAR(15)))))
+        INSERT ##Hashes (Cnter, Hash) VALUES (@Counter, sys.fn_sqlvarbasetostr(HASHBYTES('MD5', @RoomID + CAST(@Counter AS VARCHAR(15)))))
+        UPDATE ##Hashes SET Pos = SUBSTRING(Hash, 8, 1) WHERE Pos IS NULL
 
     END
 
     SET @Counter = @Counter + 1
 END
 
-SELECT *, SUBSTRING(Hash, 8, 1), SUBSTRING(Hash, 9, 1) FROM ##Hashes ORDER BY SUBSTRING(Hash, 8, 1), Counter
+SELECT [0] + [1] + [2] + [3] + [4] + [5] + [6] + [7] AS Part1
+FROM (
+    SELECT TOP 8 ROW_NUMBER() OVER (ORDER BY (SELECT 0)) - 1 AS SeqNr
+    ,      SUBSTRING(Hash, 8, 1) AS Val
+    FROM ##Hashes 
+) T
+PIVOT (
+    MAX(Val) FOR SeqNr IN ([0], [1], [2], [3], [4], [5], [6], [7])
+) PVT
 
 
---DROP TABLE ##Hashes
 
---4543c154 is correct for part 1
+SELECT [0] + [1] + [2] + [3] + [4] + [5] + [6] + [7] AS Part2
+FROM (
+    SELECT SUBSTRING(Hash, 8, 1) AS SeqNr
+    ,      FIRST_VALUE(SUBSTRING(Hash, 9, 1)) OVER (PARTITION BY SUBSTRING(Hash, 8, 1) ORDER BY Cnter) AS Val
+    FROM ##Hashes 
+    WHERE SUBSTRING(Hash, 8, 1) BETWEEN '0' AND '7'
+) T
+PIVOT (
+    MAX(Val) FOR SeqNr IN ([0], [1], [2], [3], [4], [5], [6], [7])
+) PVT
 
+DROP TABLE ##Hashes
 
---1050cbbd is correct for part 2
+--Runtime 00:15:35
--- 2016/Day06.sql
+++ 2016/Day06.sql
@@ -2,89 +2,41 @@ use Test_WME
 
 SET NOCOUNT ON
 
-CREATE TABLE ##Input (Line NVARCHAR(MAX));
+DECLARE @year VARCHAR(4) = '2016'
+DECLARE @day VARCHAR(2)  = '6'
 
-BULK INSERT ##Input
-FROM 'D:\Wilfred\AdventOfCode\2016\input04.txt'
-WITH (ROWTERMINATOR = '0x0A');
+EXEC dbo.GetInput @year = @year, @day = @day
+EXEC dbo.ParseInput @year = @year, @day = @day
 
+--SELECT TOP 10 * FROM ##InputNumbered
+--SELECT TOP 10 * FROM ##InputGrid
+--SELECT TOP 10 * FROM ##InputInts
+--SELECT TOP 10 * FROM ##InputSplit
+--SELECT * FROM ##Input
 
-CREATE TABLE ##Rooms (ID INT IDENTITY(1,1), RoomNr INT, RoomID VARCHAR(100), SectorID INT, Checksum VARCHAR(10))
-
-INSERT ##Rooms (RoomNr, RoomID, SectorID, Checksum)
-SELECT ROW_NUMBER() OVER (ORDER BY (SELECT 0)) AS RoomNr
-,      SUBSTRING(Line, 1, LEN(Line) - CHARINDEX('-', REVERSE(Line))) AS RoomID
-,      REVERSE(LEFT(REVERSE(SUBSTRING(Line, 1, CHARINDEX('[', Line) - 1)), CHARINDEX('-', REVERSE(SUBSTRING(Line, 1, CHARINDEX('[', Line) - 1))) - 1)) AS SectorID
-,      SUBSTRING(Line, CHARINDEX('[', Line) + 1, CHARINDEX(']', Line) - CHARINDEX('[', Line) - 1) AS Checksum     
-FROM ##Input
-
-;WITH cte_Letters AS (
-    SELECT RoomNr
-    ,      1 AS LetterNr
-    ,      LEFT(RoomID, 1) AS Letter
-    ,      SUBSTRING(RoomID, 2, LEN(RoomID)) AS Remainder
-    FROM ##Rooms
-    UNION ALL
-    SELECT RoomNr
-    ,      LetterNr + 1
-    ,      LEFT(Remainder, 1) AS Letter
-    ,      SUBSTRING(Remainder, 2, LEN(Remainder)) AS Remainder
-    FROM cte_Letters
-    WHERE LEN(Remainder) > 0
-), cte_Top5 AS (
-    SELECT RoomNr, Letter, COUNT(1) AS Amount, ROW_NUMBER() OVER (PARTITION BY RoomNr ORDER BY RoomNr, COUNT(1) DESC, Letter) AS Top5
-    FROM cte_Letters
-    WHERE Letter <> '-'
-    GROUP BY RoomNr, Letter
-), cte_Comparision AS (
-    SELECT L1.RoomNr
-    ,      L1.Letter + L2.Letter + L3.Letter + L4.Letter + L5.Letter AS RealChecksum
-    ,      R.Checksum 
-    ,      CASE WHEN L1.Letter + L2.Letter + L3.Letter + L4.Letter + L5.Letter = R.Checksum THEN SectorID ELSE 0 END AS RealRoom
-    FROM cte_Top5 L1
-    INNER JOIN cte_Top5 L2 ON L1.RoomNr = L2.RoomNr AND L1.Top5 = L2.Top5 - 1
-    INNER JOIN cte_Top5 L3 ON L2.RoomNr = L3.RoomNr AND L2.Top5 = L3.Top5 - 1
-    INNER JOIN cte_Top5 L4 ON L3.RoomNr = L4.RoomNr AND L3.Top5 = L4.Top5 - 1
-    INNER JOIN cte_Top5 L5 ON L4.RoomNr = L5.RoomNr AND L4.Top5 = L5.Top5 - 1
-    INNER JOIN ##Rooms R ON L1.RoomNr = R.RoomNr
-    WHERE L1.Top5 = 1
+;WITH cte_Cnt AS (
+    SELECT ColNr, Val, COUNT(1) AS Cnt FROM ##InputGrid GROUP BY ColNr, Val
 )
-SELECT SUM(RealRoom) FROM cte_Comparision
+SELECT [0] + [1] + [2] + [3] + [4] + [5] + [6] + [7] AS Part1
+FROM (
+    SELECT c.ColNr, c.Val
+    FROM cte_Cnt c
+    INNER JOIN (SELECT ColNr, MAX(Cnt) AS MaxCnt FROM cte_Cnt GROUP BY ColNr) T ON c.ColNr = T.ColNr AND c.Cnt = T.MaxCnt
+) U
+PIVOT (
+    MAX(Val) FOR ColNr IN ([0],[1],[2],[3],[4],[5],[6],[7])
+) PVT
 
---173787 Is correct for part 1
 
-
-;WITH cte_Letters AS (
-    SELECT RoomNr
-    ,      1 AS LetterNr
-    ,      LEFT(RoomID, 1) AS Letter
-    ,      SUBSTRING(RoomID, 2, LEN(RoomID)) AS Remainder
-    ,      SectorID
-    FROM ##Rooms
-    UNION ALL
-    SELECT RoomNr
-    ,      LetterNr + 1
-    ,      LEFT(Remainder, 1) AS Letter
-    ,      SUBSTRING(Remainder, 2, LEN(Remainder)) AS Remainder
-    ,      SectorID
-    FROM cte_Letters
-    WHERE LEN(Remainder) > 0
-)--, cte_Corrected AS (
-SELECT RoomNr, Letter, LetterNr
-,      CASE WHEN Letter = '-' THEN ' '
-            WHEN ASCII(Letter) + SectorID % 26 > 122 THEN CHAR(ASCII(Letter) + SectorID % 26 - 26) ELSE CHAR(ASCII(Letter) + SectorID % 26) END AS RotatedLetter
-FROM cte_Letters
-WHERE RoomNr = 300
-ORDER BY RoomNr, LetterNr
---)
---SELECT * FROM cte_Corrected WHERE RotatedLetter = 'n' AND LetterNr = 1
-
-SELECT * FROM ##Rooms WHERE RoomNr = 300
-
-/*
-
-DROP TABLE ##Rooms
-DROP TABLE ##Input
-
-*/
-
+;WITH cte_Cnt AS (
+    SELECT ColNr, Val, COUNT(1) AS Cnt FROM ##InputGrid GROUP BY ColNr, Val
+)
+SELECT [0] + [1] + [2] + [3] + [4] + [5] + [6] + [7] AS Part2
+FROM (
+    SELECT c.ColNr, c.Val
+    FROM cte_Cnt c
+    INNER JOIN (SELECT ColNr, MIN(Cnt) AS MinCnt FROM cte_Cnt GROUP BY ColNr) T ON c.ColNr = T.ColNr AND c.Cnt = T.MinCnt
+) U
+PIVOT (
+    MAX(Val) FOR ColNr IN ([0],[1],[2],[3],[4],[5],[6],[7])
+) PVT
