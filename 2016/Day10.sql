c2efd4f8086dc2730611f669eb2b3ff236128320
Wilfred Meyboom <A-Wilfred.Meyboom@paccar.com>
2023-02-23 16:29:35 +0100

    2016 Day 9 and 10 cleanup

--- 2016/Day10.sql
+++ 2016/Day10.sql
@@ -1,106 +1,93 @@
-use Test_WME
+USE Test_WME
 
 SET NOCOUNT ON
 
-CREATE TABLE ##Input (Line NVARCHAR(MAX));
+DECLARE @year VARCHAR(4) = '2016'
+DECLARE @day VARCHAR(2)  = '10'
 
-BULK INSERT ##Input
-FROM 'D:\Wilfred\AdventOfCode\2016\input10.txt'
-WITH (ROWTERMINATOR = '0x0A');
+EXEC dbo.GetInput @year = @year, @day = @day
+EXEC dbo.ParseInput @year = @year, @day = @day
 
-CREATE TABLE ##Bots (ID INT IDENTITY(1,1), BotNr INT, FirstValue INT, SecondValue INT, HighToBotNr INT, LowToBotNr INT)
+--SELECT TOP 10 * FROM ##InputNumbered
+--SELECT TOP 10 * FROM ##InputGrid
+--SELECT TOP 10 * FROM ##InputInts
+--SELECT TOP 100 * FROM ##InputSplit
+--SELECT * FROM ##Input
 
-UPDATE ##Input
-SET Line = REPLACE(Line, 'gives low to output 0', 'gives low to output 100')
 
-;WITH cte_Scrubbed AS (
-    SELECT REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(Line, ' and high to bot ', '|'), ' gives low to bot ', '_'), 'bot ', ''), ' gives low to output ', '_-'), ' and high to output ', '|-') AS ScrubbedLine
-    FROM ##Input
-    WHERE LEN(Line) > 30
-)
-INSERT ##Bots (BotNr, LowToBotNr, HighToBotNr)
-SELECT LEFT(ScrubbedLine, CHARINDEX('_', ScrubbedLine) - 1) AS BotNr
-,      SUBSTRING(ScrubbedLine, CHARINDEX('_', ScrubbedLine) + 1, CHARINDEX('|', ScrubbedLine) - CHARINDEX('_', ScrubbedLine) - 1) AS LowToBotNr
-,      SUBSTRING(ScrubbedLine, CHARINDEX('|', ScrubbedLine) + 1, LEN(ScrubbedLine)) AS HighToBotNr
-FROM cte_Scrubbed
+CREATE TABLE ##Bots (ID INT IDENTITY(1,1), BotNr INT, LowType VARCHAR(10), LowToNr INT, HighType VARCHAR(10), HighToNr INT, FirstValue INT, SecondValue INT)
 
-;WITH cte_Start AS (
-    SELECT SUBSTRING(Line, 7, CHARINDEX(' goes to bot ', Line) - 7) AS Value
-    ,      SUBSTRING(Line, CHARINDEX(' goes to bot ', Line) + 13, LEN(Line)) AS Bot
-    FROM ##Input WHERE LEN(Line) < 30
+INSERT ##Bots(BotNr, LowType, LowToNr, HighType, HighToNr)
+SELECT [2] AS Bot, [6] AS LowDestType, [7] AS LowDest, [11] AS HighDestType, [12] AS HighDest FROM (
+    SELECT RowNr, PieceNr, Piece
+    FROM ##InputSplit
+    WHERE PieceNr IN (2, 6, 7, 11, 12) AND RowNr IN (SELECT RowNr FROM ##InputSplit WHERE PieceNr = 1 AND Piece = 'bot')
+) T
+PIVOT (
+    MAX(Piece) FOR PieceNr IN ([2],[6],[7],[11],[12])
+) PVT
+
+;WITH cte_StartingValues AS (
+    SELECT [2] AS Val, [6] AS ToBot
+    FROM (
+        SELECT RowNr, PieceNr, Piece
+        FROM ##InputSplit
+        WHERE RowNr IN (SELECT RowNr FROM ##InputSplit WHERE PieceNr = 1 AND Piece = 'value')
+    ) T
+    PIVOT (
+        MAX(Piece) FOR PieceNr IN ([2],[6])
+    ) PVT
 )
 UPDATE B
-SET B.FirstValue = cS.Value
-,   B.SecondValue = cS2.Value
+SET FirstValue = c1.Val
+,   SecondValue = c2.Val
 FROM ##Bots B
-INNER JOIN cte_Start cS ON B.BotNr = cS.Bot
-LEFT JOIN cte_Start cS2 ON B.BotNr = cS2.Bot AND cS.Value <> cS2.Value
+INNER JOIN cte_StartingValues c1 ON B.BotNr = c1.ToBot
+LEFT JOIN cte_StartingValues c2 ON B.BotNr = c2.ToBot AND c1.Val <> c2.Val
+WHERE c2.Val IS NULL OR c1.Val < c2.Val
 
-
-DECLARE @Counter INT = 0
-
-WHILE EXISTS(SELECT 1 FROM ##Bots WHERE FirstValue IS NULL OR SecondValue IS NULL) AND @Counter < 500
+WHILE (SELECT COUNT(1) FROM ##Bots WHERE FirstValue IS NULL OR SecondValue IS NULL) > 0
 BEGIN
-
-    ;WITH cte_BotValues AS (
-        SELECT B.HighToBotNr AS BotNr
-        ,      CASE WHEN B.FirstValue > B.SecondValue THEN B.FirstValue ELSE B.SecondValue END AS Value
-        FROM ##Bots B
-        WHERE B.FirstValue IS NOT NULL AND B.SecondValue IS NOT NULL
-        UNION 
-        SELECT B.LowToBotNr
-        ,      CASE WHEN B.FirstValue < B.SecondValue THEN B.FirstValue ELSE B.SecondValue END AS Value
-        FROM ##Bots B
-        WHERE B.FirstValue IS NOT NULL AND B.SecondValue IS NOT NULL
-        UNION
-        SELECT BotNr
-        ,      FirstValue
+    
+    ;WITH cte_NewValues AS (
+        SELECT LowToNr AS DestBot
+        ,      CASE WHEN FirstValue < SecondValue THEN FirstValue ELSE SecondValue END AS ValueForBot
         FROM ##Bots 
-        WHERE FirstValue IS NOT NULL
+        WHERE FirstValue IS NOT NULL AND SecondValue IS NOT NULL AND LowType = 'bot'
         UNION
-        SELECT BotNr
-        ,      SecondValue
+        SELECT HighToNr AS DestBot
+        ,      CASE WHEN FirstValue > SecondValue THEN FirstValue ELSE SecondValue END AS ValueForBot
         FROM ##Bots 
-        WHERE SecondValue IS NOT NULL
-    ), cte_Numbered AS (
-        SELECT ROW_NUMBER() OVER (Partition BY BotNr ORDER BY Value) AS Nr
-        ,      BotNr
-        ,      Value
-        FROM cte_BotValues
+        WHERE FirstValue IS NOT NULL AND SecondValue IS NOT NULL AND HighType = 'bot'
+        UNION
+        SELECT BotNr AS DestBot, FirstValue AS ValueForBot
+        FROM ##Bots
+        WHERE FirstValue IS NOT NULL 
     )
     UPDATE B
-    SET B.FirstValue = V1.Value
-    ,   B.SecondValue = V2.Value
+    SET FirstValue = cN.ValueForBot
+    ,   SecondValue = cN2.ValueForBot
     FROM ##Bots B
-    LEFT JOIN cte_Numbered V1 ON B.BotNr = V1.BotNr AND V1.Nr = 1
-    LEFT JOIN cte_Numbered V2 ON B.BotNr = V2.BotNr AND V2.Nr = 2
-    
-    SET @Counter = @Counter + 1
-
-    PRINT 'Rows updated: ' + CAST(@@ROWCOUNT AS VARCHAR(10))
+    INNER JOIN cte_NewValues cN ON B.BotNr = cN.DestBot
+    INNER JOIN cte_NewValues cN2 ON B.BotNr = cN2.DestBot AND cN.ValueForBot < cN2.ValueForBot
 
 END
 
+SELECT BotNr AS Part1 FROM ##Bots WHERE FirstValue IN (17,61) AND SecondValue IN (17,61)
 
-SELECT * FROM ##Bots WHERE FirstValue + SecondValue = (61+17) 
--- 47 is correct for part 1
-
-SELECT * FROM ##Bots WHERE LowToBotNr BETWEEN -2 AND 0 OR LowToBotNr = -100
-
-/*
+SELECT [0] * [1] * [2] AS Part2
+FROM (
+    SELECT CASE WHEN LowType = 'output' THEN LowToNr ELSE HighToNr END AS OutputNr
+    ,      CASE WHEN LowType = 'output' AND FirstValue < SecondValue 
+                  OR HighType = 'output' AND FirstValue > SecondValue 
+                THEN FirstValue ELSE SecondValue END AS Val
+    FROM ##Bots
+    WHERE (LowType = 'output' AND LowToNr IN (0,1,2)) 
+       OR (HighType = 'output' AND HighToNr IN (0,1,2)) 
+) T
+PIVOT (
+    MAX(Val) FOR OutputNr IN ([0],[1],[2])
+) PVT
 
 DROP TABLE ##Bots
-DROP TABLE ##Input
-
-*/
-
-SELECT * FROM ##Input ORDER BY 1
-
---> Output 0 = 2
---> Output 1 = 41
---> Output 2 = 61
-
-SELECT 2* 31 * 43
---5002 is too high
---2666 is correct for part 2
 
