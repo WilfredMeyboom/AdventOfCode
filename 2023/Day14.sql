539bcfc7acf3bc53dd41611eab215c6687464d62
Wilfred Meyboom <A-Wilfred.Meyboom@paccar.com>
2024-01-08 16:33:53 +0100

    2023, Day 14 complete

--- 2023/Day14.sql
+++ 2023/Day14.sql
@@ -18,21 +18,25 @@ CREATE UNIQUE INDEX IX_InputGrid_UQ ON ##InputGrid(RowNr,ColNr)
 
 DECLARE @Count INT = 1
 
+-- Tilt the grid one way (to the north)
 WHILE @Count > 0
 BEGIN
     
+    -- Mark every space that is empty and directly north of a rock
     UPDATE I1
     SET I1.Val = 'X'
     FROM ##InputGrid I1
     INNER JOIN ##InputGrid I2 ON I1.RowNr = I2.RowNr - 1 AND I1.ColNr = I2.ColNr
     WHERE I1.Val = '.' AND I2.Val = 'O'
 
+    -- Empty every space that has a rock and an empty space directly north of it
     UPDATE I1
     SET I1.Val = '.'
     FROM ##InputGrid I1
     INNER JOIN ##InputGrid I2 ON I1.RowNr = I2.RowNr + 1 AND I1.ColNr = I2.ColNr
     WHERE I1.Val = 'O' AND I2.Val = 'X'
 
+    -- Change the earlier mark to a rock
     UPDATE I1
     SET I1.Val = 'O'
     FROM ##InputGrid I1
@@ -45,9 +49,11 @@ DECLARE @MaxRowNr INT
 SELECT @MaxRowNr = MAX(RowNr) FROM ##InputGrid
 SELECT SUM(@MaxRowNr - RowNr + 1) AS Part1 FROM ##InputGrid WHERE Val = 'O'
 
+-- We expect some kind of repetion in the results so prepare to store the result after each cycle
 CREATE TABLE ##Result (ID INT IDENTITY, Result INT)
 
---north, then west, then south, then east
+-- Move grid
+-- First north, then west, then south, then east
 
 DECLARE @Cycle INT = 0
 DECLARE @NrOfBoulders INT
@@ -57,7 +63,7 @@ BEGIN
     
     SET @Count = 1
 
-    --North
+    -- Tilt the grid to the north
     WHILE @Count > 0
     BEGIN
     
@@ -83,7 +89,7 @@ BEGIN
 
     SET @Count = 1
 
-    --West
+    -- Tilt the grid to the west
     WHILE @Count > 0
     BEGIN
     
@@ -109,7 +115,7 @@ BEGIN
 
     SET @Count = 1
 
-    --South
+    -- Tilt the grid to the south
     WHILE @Count > 0
     BEGIN
     
@@ -135,7 +141,7 @@ BEGIN
 
     SET @Count = 1
 
-    --East
+    -- Tilt the grid to the east
     WHILE @Count > 0
     BEGIN
     
@@ -159,6 +165,7 @@ BEGIN
         SET @Count = @@ROWCOUNT
     END
 
+    -- Store the result as if this was the end 
     INSERT ##Result (Result)
     SELECT SUM(@MaxRowNr - RowNr + 1) FROM ##InputGrid WHERE Val = 'O'
 
@@ -166,12 +173,25 @@ BEGIN
 
 END
 
-SELECT R2.ID - R1.ID
+/*
+-- Let's check if the solution starts repeating...
+
+SELECT R2.ID - R1.ID, *
 FROM ##Result R1
 INNER JOIN ##Result R2 ON R1.Result = R2.Result AND R1.ID < R2.ID
 ORDER BY R1.ID
 
+-- Ah, yes, from iteration 153 and onward. The solutions repeat with a frequency of 26 solutions
 
-SELECT 1000000000 % 26 + ((153 / 26) + 1) * 26
-SELECT (1000000000 - 168) % 26
-SELECT Result AS Part2 FROM ##Result WHERE ID = 168
+*/
+
+DECLARE @Target BIGINT = 1000000000 
+DECLARE @Frequency INT = 26
+DECLARE @Threshold INT = 153
+DECLARE @SolutionID INT 
+
+-- Reduce the target to the minimum value (= 12) then add the frequency enough times to increase the value to be above the threshold.
+-- So solution ID = 168
+SELECT @SolutionID = @Target % @Frequency + CEILING(1.0 * @Threshold / @Frequency) * @Frequency
+
+SELECT Result AS Part2 FROM ##Result WHERE ID = @SolutionID
